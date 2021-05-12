827b82c52259de1613d526723f0115f13d405ca4
LTK1102 <83080791+LTK1102@users.noreply.github.com>
2021-05-12 17:17:05 +0200

    Add files via upload
    fixed "new_account"
    new function: "changing_dispo"

--- Konto.py
+++ Konto.py
@@ -28,15 +28,15 @@ class Account:
             zielkonto.deposit(betrag)
 
 
-mein_Konto = Account("00001", 5000, 500)
+mein_Konto = Account("1", 5000, 500)
 
 
-mein_Zweitkonto = Account("00002", 500, 1500)
+mein_Zweitkonto = Account("2", 500, 1500)
 
 
 Konten = {
-    "00002": mein_Zweitkonto,
-    "00001": mein_Konto,
+    "2": mein_Zweitkonto,
+    "1": mein_Konto,
 
 }
 
@@ -44,11 +44,13 @@ Konten = {
 kunde = ""
 
 def start():
-    global input1
+    print("\nUm ein neues Konto zu erstellen geben Sie bitte \"1\" ein.")
+    print("Wenn Sie bereits ein Konto besitzen, dann geben sie bitte \"2\" ein, um zum Login weitergeleitet zu werden.")
+    input1 = input("Eingabe: ")
     global kunde
     if input1 == "1":
         new_account()
-        login()
+        kunde = login()
         menue()
     elif input1 == "2":
         kunde = login()
@@ -56,6 +58,7 @@ def start():
 
 
 def new_account():
+    print("\nEs wird ein neuer Account angelegt.")
     new_accountnumber = str(len(Konten) + 1)
     new_accountvariable = Account(new_accountnumber, 0, 0)
     Konten[str(new_accountnumber)] = new_accountvariable
@@ -64,9 +67,29 @@ def new_account():
     print("Bitte notieren Sie sich diese und melden Sie sich mit dieser erneut an.")
 
 
+def changing_dispo():
+    global kunde
+    print("\nIhr aktueller Dipositionskredit beträgt " + str(kunde.dispo) + "€.")
+    print("Sie können einen Dispositionskredit von bis zu 300€ einrichten.")
+    print("Geben Sie bitte den Kredit in gewünschter Höhe an.")
+    new_dispo = input("Eingabe: ")
+    try:
+        new_dispo = float(new_dispo)
+    except TypeError:
+        print("Bitte geben Sie eine Zahl von 0 bis 300 ein.")
+        changing_dispo()
+    while new_dispo < 0 or new_dispo > 300:
+        print("Bitte geben Sie eine Zahl von 0 bis 300 ein.")
+        changing_dispo()
+    kunde.dispo = new_dispo
+    print("Ihr Dispositionskredit wurde erfolgreich auf " + str(kunde.dispo) + "€ aktualisiert.")
+    menue()
+
+
+
 
 def login():
-    print("(Anmerkung: Debug-Kontonummern sind \"00001\" und \"00002\".)")
+    print("(Anmerkung: Debug-Kontonummern sind \"1\" und \"2\".)")
     kontonummer = input("Bitte geben Sie Ihre Kontonummer ein: ")
     while Konten.get(kontonummer) == None:
         print("Kontonummer ist ungültig.")
@@ -75,7 +98,7 @@ def login():
 
 
 def input_valid_account():
-    print("(Anmerkung: Debug-Kontonummern sind \"00001\" und \"00002\".)")
+    print("(Anmerkung: Debug-Kontonummern sind \"1\" und \"2\".)")
     nummer = input()
     while Konten.get(nummer) == None:
         print("Kontonummer ungültig. Versuchen Sie es erneut.")
@@ -91,7 +114,8 @@ def menue():
     print("2. Betrag einzahlen")
     print("3. Betrag überweisen")
     print("4. Kontoinfo anzeigen")
-    print("5. Konto wechseln")
+    print("5. Dispositionskredit ändern")
+    print("6. Konto wechseln / neues Konto eröffnen")
     eingabe = input("Eingabe: ")
     if eingabe == "1":
         print("Geben Sie den abzubuchenden Betrag an:")
@@ -140,7 +164,10 @@ def menue():
         print("Dispo: " + str(kunde.dispo) + "€\n")
         menue()
     elif eingabe == "5":
-        kunde = login()
+        changing_dispo()
+        menue()
+    elif eingabe == "6":
+        start()
         menue()
     else:
         print("Ihre Eingabe ist ungültig. Bitte versuchen Sie es erneut.")
@@ -148,8 +175,4 @@ def menue():
 
 
 print("Herzlich willkommen zum Bankingsimulator.")
-print("Um ein neues Konto zu erstellen geben Sie bitte \"1\" ein.")
-print("Wenn Sie bereits ein Konto besitzen, dann geben sie bitte \"2\" ein, um zum Login weitergeleitet zu werden.")
-input1 = input("Eingabe: ")
-
 start()
